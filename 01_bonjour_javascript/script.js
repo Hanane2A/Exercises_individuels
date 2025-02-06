4034bc49f9424e98b263fb340bc500b86d00d2be
Hanane <benabid.hanane@gmail.com>
2025-02-06 10:21:32 +0100

    ajout du premier exercice individuel

---
+++ 01_bonjour_javascript/script.js
@@ -0,0 +1,16 @@
+// console.log("Etape2");
+
+// let firstname = "Beyonce";
+// let message = "Bonjour " + firstname + " !"
+// console.log(message);
+// console.log("=======");
+// console.log("Etape3");
+
+
+function sayHello(firstname){
+    let message = "Bonjour " + firstname + " !"
+    console.log(message);
+}
+
+let beyonce = "Beyonce"
+sayHello(beyonce)
