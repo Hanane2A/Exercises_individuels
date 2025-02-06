c66ec61e0cd829f061a8288944ec57bca2362440
Hanane <benabid.hanane@gmail.com>
2025-02-06 14:37:36 +0100

    exercie_individuel

--- 01_bonjour_javascript/script.js
+++ 01_bonjour_javascript/script.js
@@ -3,14 +3,34 @@
 // let firstname = "Beyonce";
 // let message = "Bonjour " + firstname + " !"
 // console.log(message);
-// console.log("=======");
-// console.log("Etape3");
+//
 
+// Etape 3
 
-function sayHello(firstname){
-    let message = "Bonjour " + firstname + " !"
+// function sayHello(firstname){
+//     let message = "Bonjour " + firstname + " !"
+//     console.log(message);
+// }
+// let beyonce = "Beyonce"
+// sayHello(beyonce)
+
+// Etape 4
+
+function sayHello(firstname , hour){
+
+    let message = "Bonjour " 
+    message = "Bonjour " + firstname + " !"
+
+    if (hour>= 18)
+        console.log("Bonsoir " + firstname + "!")
+    else 
+
     console.log(message);
+
 }
 
 let beyonce = "Beyonce"
-sayHello(beyonce)
+
+sayHello(`Beyonce`, 11);
+sayHello(`Beyonce`, 18);
+sayHello(`Beyonce`, 17);
